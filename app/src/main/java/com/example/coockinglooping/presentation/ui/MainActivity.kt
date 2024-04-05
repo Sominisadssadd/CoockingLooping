a6be842b1bafce2b85d18aed6d771adb11000669
zinak <zinakola2004@gmail.com>
2024-04-05 10:58:36 +0500

    release project structure

--- app/src/main/java/com/example/coockinglooping/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/coockinglooping/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.coockinglooping
+package com.example.coockinglooping.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coockinglooping.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
